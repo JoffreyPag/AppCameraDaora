6534e0daed607a24dc5a2a859ad673db0805d8b4
JoffreyPag <joffreypag@gmail.com>
2017-10-06 23:46:44 -0300

    começo

--- app/src/main/java/com/example/joffr/appcameradaora/MainActivity.java
+++ app/src/main/java/com/example/joffr/appcameradaora/MainActivity.java
@@ -1,27 +1,63 @@
 package com.example.joffr.appcameradaora;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button camBanco, camExt, camInt, pegaBanco, pegaExt, pegaInt;
+    Button butCam, butpegaimg;
     ImageView imagemTela;
+    private static final int TIRAR_FOTO = 1010;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        camBanco = (Button)findViewById(R.id.button1);
-        pegaBanco = (Button)findViewById(R.id.button2);
-        camExt = (Button)findViewById(R.id.button3);
-        pegaExt = (Button)findViewById(R.id.button4);
-        camInt = (Button)findViewById(R.id.button5);
-        pegaInt = (Button)findViewById(R.id.button6);
+        butCam = (Button) findViewById(R.id.butao);
+        butpegaimg = (Button) findViewById(R.id.button2);
 
-        imagemTela = (ImageView)findViewById(R.id.ultimaimagem);
+        imagemTela = (ImageView) findViewById(R.id.imagemTela);
+    }
+
+    public void teste(View view) {
+        chamaCamera();
+    }
+
+    public void chamaCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, TIRAR_FOTO);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == TIRAR_FOTO) {
+            if (resultCode == RESULT_OK) {
+                if (data != null) {
+                    Bundle bundle = data.getExtras();
+                    Bitmap bitmap = (Bitmap) bundle.get("data");
+                } else if (resultCode == RESULT_CANCELED) {
+                    Toast.makeText(this, "Cancelado", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "cam fechada", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
 }
